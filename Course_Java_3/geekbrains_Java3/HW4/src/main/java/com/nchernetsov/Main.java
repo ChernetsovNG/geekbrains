3f2093afa7d660ebfad48368e6f0f9f225da09cd
ChernetsovNG <n.chernetsov86@gmail.com>
2017-08-25 20:21:56 +0300

    HW4: добавлена вторая часть ДЗ с параллельной записью в файл из 3-х потоков

--- Course_Java_3/geekbrains_Java3/HW4/src/main/java/com/nchernetsov/Main.java
+++ Course_Java_3/geekbrains_Java3/HW4/src/main/java/com/nchernetsov/Main.java
@@ -1,9 +1,16 @@
 package com.nchernetsov;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     private final Object sync = new Object();
     private final int nTimesToWrite = 5;
     private volatile char currentLetter = 'A';
+
+    private final RandomString randomString = new RandomString(6);
 
     public static void main(String[] args) {
         // 1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз,
@@ -17,6 +24,36 @@ public class Main {
         threadA.start();
         threadB.start();
         threadC.start();
+
+        try {
+            threadA.join();
+            threadB.join();
+            threadC.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 2. Написать метод, в котором 3 потока построчно пишут данные в файл
+        // (по​ ​10​ ​записей,​ ​с ​периодом​ в ​20​ ​мс)
+        try {
+            FileOutputStream fos = new FileOutputStream("test_file.txt");
+
+            Thread writer1 = new Thread(() -> mainObject.printDataIntoFile(fos, 10));
+            Thread writer2 = new Thread(() -> mainObject.printDataIntoFile(fos, 10));
+            Thread writer3 = new Thread(() -> mainObject.printDataIntoFile(fos, 10));
+
+            writer1.start();
+            writer2.start();
+            writer3.start();
+
+            writer1.join();
+            writer2.join();
+            writer3.join();
+
+            fos.close();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void printLetter(char letterToPrint, char nextLetter) {
@@ -31,6 +68,19 @@ public class Main {
                     sync.notifyAll();
                 }
             } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printDataIntoFile(FileOutputStream fileOutputStream, int countStringsToWrite) {
+        for (int i = 0; i < countStringsToWrite; i++) {
+            try {
+                String string = randomString.nextString();
+                fileOutputStream.write(string.getBytes());
+                fileOutputStream.write("|".getBytes());  // пишем разделитель
+                TimeUnit.MILLISECONDS.sleep(20);
+            } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             }
         }
