58d7e407dc35b6e8348e6f6e0fe0b9021615d91e
ChernetsovNG <n.chernetsov86@gmail.com>
2018-06-11 15:16:04 +0300

    Spring-1: HW5: добавлен минимальный вариант ДЗ

---
+++ Spring-1/HW5/src/main/java/ru/nchernetsov/controller/HelloController.java
@@ -0,0 +1,17 @@
+package ru.nchernetsov.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
