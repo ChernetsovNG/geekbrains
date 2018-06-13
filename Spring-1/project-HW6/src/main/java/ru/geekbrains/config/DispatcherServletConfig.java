f2a0b5aa4bd9cc12e939e27feb1fa1a8c51f5079
nchernetsov <n.chernetsov86@gmail.com>
2018-06-13 20:25:11 +0300

    Spring-1: project-HW6: add elements from HW5

---
+++ Spring-1/project-HW6/src/main/java/ru/geekbrains/config/DispatcherServletConfig.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.geekbrains.controller")
+public class DispatcherServletConfig implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+}
